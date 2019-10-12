5e499229685f1ed94fad8987b8a64529922c9d63
Rambhau Waykar <ramwaykar@gmail.com>
2019-10-12 15:50:00 +0530

    completed code

--- src/components/AddMovieForm/AddMovieForm.js
+++ src/components/AddMovieForm/AddMovieForm.js
@@ -6,6 +6,70 @@ import FormHeader from "../FormHeader/FormHeader";
 import "./AddMovieForm.css";
 
 class AddMovieForm extends React.Component {
+  constructor(props){
+    super();
+
+    this.state = {MovieName:'', Genre: 'Science Fiction', rating: '', Website: ''};
+    this.submit = this.submit.bind(this);
+    this.nameChange = this.nameChange.bind(this);
+    this.genreChange = this.genreChange.bind(this);
+    this.ratingChange = this.ratingChange.bind(this);
+    this.websiteChange = this.websiteChange.bind(this);
+  }
+
+  submit = (e) => {
+    e.preventDefault();    
+    if(this.state.MovieName === ''){
+      alert("Please enter movie name");
+      return false;
+    }else if(!(this.state.MovieName.match(/^[A-Za-z]+$/))){
+      alert("Please enter correct movie name, use alphabets only");
+      return false;
+    }else if(this.state.Genre === ''){
+      alert("Please enter Genre");
+      return false;
+    }else if(this.state.rating === ''){
+      alert("Please enter rating");
+      return false;
+    }   
+
+    if(this.state.Website !== ''){
+      var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+      if (!re.test(this.state.Website)) { 
+          alert("Please enter valid Website");
+          return false;
+      }
+    }
+    
+
+    var data = {
+      MovieName:this.state.MovieName, 
+      Genre: this.state.Genre, 
+      rating: this.state.rating, 
+      Website: this.state.Website
+    }    
+    //localStorage.removeItem('movieList');
+    var oldItems = JSON.parse(localStorage.getItem('movieList')) || [];
+    oldItems.push(data);
+    localStorage.setItem('movieList', JSON.stringify(oldItems));
+  }
+
+  nameChange = (e) => {
+    this.setState({MovieName: e.target.value});
+  }
+
+  genreChange = (e) => {
+    this.setState({Genre: e.target.value});
+  }
+
+  ratingChange = (e) => {
+    this.setState({rating: e.target.value});
+  }
+
+  websiteChange = (e) => {
+    this.setState({Website: e.target.value});
+  }
+
   render() {
     return (
       <div className="row">
@@ -13,7 +77,7 @@ class AddMovieForm extends React.Component {
         <button
           className="add-movie-button"
           onClick={() => {
-
+            window.location = window.location.href.split("/add-movie")[0]
           }}
         >
           <i className="fa fa-long-arrow-left" aria-hidden="true" />
@@ -24,13 +88,13 @@ class AddMovieForm extends React.Component {
               <label className="label" htmlFor="MovieName">
                 Movie Name*
               </label>
-              <input id="MovieName" name="MovieName" type="text" tabIndex="1" />
+              <input id="MovieName" name="MovieName" type="text" tabIndex="1" required onChange={this.nameChange}/>
             </div>
             <div className="wrapper">
               <label className="label" id="Genre" htmlFor="Genre">
                 Genre*
               </label>
-              <select name="Genre">
+              <select name="Genre" required onChange={this.genreChange} defaultValue='Science Fiction'>
                 <option value="Science Fiction">Science Fiction</option>
                 <option value="Drama">Drama</option>
                 <option value="Action">Action</option>
@@ -44,17 +108,17 @@ class AddMovieForm extends React.Component {
               <label className="label" htmlFor="rating">
                 Rating*
               </label>
-              <input type="radio" name="rating" value="1" label="1" />1
-              <input type="radio" name="rating" value="2" />2
-              <input type="radio" name="rating" value="3" />3
-              <input type="radio" name="rating" value="4" />4
-              <input type="radio" name="rating" value="5" />5
+              <div><input type="radio" name="rating" value="1" label="1" onChange={this.ratingChange}/>1</div>
+              <div><input type="radio" name="rating" value="2"  onChange={this.ratingChange}/>2</div>
+              <div><input type="radio" name="rating" value="3"  onChange={this.ratingChange}/>3</div>
+              <div><input type="radio" name="rating" value="4"  onChange={this.ratingChange}/>4</div>
+              <div><input type="radio" name="rating" value="5"  onChange={this.ratingChange}/>5</div>
             </div>
             <div className="wrapper">
               <label className="label" htmlFor="Website">
                 Website
               </label>
-              <input id="Website" name="Website" />
+              <input id="Website" type="url" name="Website" onChange={this.websiteChange}/>
             </div>
             <div className="wrapper submit-btn">
               <input
@@ -63,6 +127,7 @@ class AddMovieForm extends React.Component {
                 type="submit"
                 value="Submit"
                 tabIndex="5"
+                onClick={this.submit}
               />
             </div>
           </form>
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -7,22 +7,28 @@ class MovieList extends Component {
   render() {
     // TODO:: The list shown should be sorted by ratings, in descending order
     // TODO:: If no data in movieList, show message - 'No movies found to display'
+    const movieList = JSON.parse(localStorage.getItem('movieList')) || [];
     const list =
-      this.props.movieList &&
-      this.props.movieList.map(movie => {
+    movieList ?
+    movieList.map(movie => {
         return (
           <div className="movie">
             {/* TODO:: Convert this div into a link (website filled), 
           on click, the link should open in new tab
           */}
-            <div>{movie.name}</div>
-            <div>({movie.genre})</div>
+            <div>{
+              movie.Website ? 
+              <a href={movie.Website} target="_blank">{movie.MovieName}</a> 
+              : movie.MovieName
+              }
+            </div>
+            <div>({movie.Genre})</div>
             <div>
               <span className="fa fa-star" /> {movie.rating}/5
             </div>
           </div>
         );
-      });
+      }) : <span>No movies found to display</span>;
     return (
       <div>
         <FormHeader formTitle="Movie list" />
